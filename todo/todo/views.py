6a9ecafd8a14a6931182b4fa34963f772440d9d9
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2025-02-18 23:17:03 +0530

    Initial commit

---
+++ todo/todo/views.py
@@ -0,0 +1,18 @@
+from django . shortcuts import render,redirect
+from django.contrib.auth.models import User
+from todo import models
+from todo.models import Todoo
+
+def signup(request):
+    if request.method=='POST':
+        fnm=request.POST.get('fnm')
+        emailid=request.POST.get('email')
+        pwd=request.POST.get('pwd')
+        print(fnm,emailid,pwd)
+        my_user=User.objects.create_user(fnm,emailid,pwd)
+
+        my_user.save()
+
+        return redirect('/login')
+
+    return render(request,"signup.html")
