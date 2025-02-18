802f872b7efa8d48515ff62179d7365d6b57d14a
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2025-02-18 23:18:03 +0530

    Initial commit

--- todo/todo/views.py
+++ todo/todo/views.py
@@ -8,7 +8,7 @@ def signup(request):
         fnm=request.POST.get('fnm')
         emailid=request.POST.get('email')
         pwd=request.POST.get('pwd')
-        print(fnm,emailid,pwd)
+        # print(fnm,emailid,pwd)
         my_user=User.objects.create_user(fnm,emailid,pwd)
 
         my_user.save()
