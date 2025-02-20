658ab4af8f8c3da3ed88b8157349dfe213a6f1ab
Shubhangi Dwivedi <shubhangi.dwivedi04@gmail.com>
2025-02-21 00:55:18 +0530

    final commit

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -20,5 +20,11 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.signup)
+    path('', views.home),
+    path('signup/',views.signup),
+    path('loginn/',views.loginn),
+    path('todopage/',views.todo),
+    path('edit_todo/<int:srno>',views.edit_todo,name='edit_todo'),
+    path('delete_todo/<int:srno>',views.delete_todo,name='edit_todo'),
+    path('signout/',views.signout,name='signout')
 ]
